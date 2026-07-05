fa4f3c3afa179deba4bd4463e405a56a70eae26e
DevSagar007 <sagorkhan9294@gmail.com>
2026-07-06 00:23:48 +0600

    add dotenv configuration and update dependencies in package.json and package-lock.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,8 +13,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://smartDBUser:hxqLULda7RiwhdgM@cluster0.uhofepr.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uhofepr.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
